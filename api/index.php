a20c4c0baddaecd75d0fc240bfea3c2a3adbd3ba
Daniel <daniel.mantuan@itixti.com.br>
2024-11-08 16:13:30 -0300

    teste

--- api/index.php
+++ api/index.php
@@ -8,7 +8,7 @@
     require_once ( __DIR__  . "/Models/tarefa.php");
 
     if(!isset($_SESSION['tarefas']) && !isset($_SESSION['error'])){
-        header('Location: ' . __DIR__  . '/Controllers/taskController.php');
+        header('Location: ' . __DIR__  . '/Controllers/taskController.php?option=1');
 
         exit;
     }
