1cbf17341260c55ac61d7a1bee0a01e90cee5a98
Daniel <daniel.mantuan@itixti.com.br>
2024-11-08 15:38:59 -0300

    teste

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@
     require_once ( __DIR__  . "/utils/dateConvert.inc.php");
     require_once (__DIR__  . "/utils/MoneyConversion.php");
 
-    ini_set('session.cookie_domain', '.vercel.app');  // Compartilha o cookie entre subdomínios
+    ini_set('session.cookie_path', '/');  // Compartilha o cookie entre subdomínios
     session_start();  // Sempre após configurar os cookies
 
     if(!isset($_SESSION['tarefas']) && !isset($_SESSION['error'])){
