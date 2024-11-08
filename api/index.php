efe875d5e259426403896f6dc97bfd1ae6104230
Daniel <daniel.mantuan@itixti.com.br>
2024-11-08 14:17:02 -0300

    fix: paths

--- api/index.php
+++ api/index.php
@@ -35,10 +35,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de tarefas</title>
-    <link rel="stylesheet" href="./Views/css/style.css">
-    <script lang="javascript" src="./Views/scripts/jquery-3.7.1.min.js"></script>
-    <script lang="javascript" src="./Views/scripts/datePicker.js"></script>
-    <script lang="javascript" src="./Views/scripts/modal.js"></script>
+    <link rel="stylesheet" href="<?= __DIR__ ?>/Views/css/style.css">
+    <script lang="javascript" src="<?= __DIR__ ?>/Views/scripts/jquery-3.7.1.min.js"></script>
+    <script lang="javascript" src="<?= __DIR__ ?>/Views/scripts/datePicker.js"></script>
+    <script lang="javascript" src="<?= __DIR__ ?>/Views/scripts/modal.js"></script>
 
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
@@ -59,7 +59,7 @@
             function alertDismissed() {
                 // Envia uma requisição para limpar a variável de sessão de erro
                 var xhr = new XMLHttpRequest();
-                xhr.open("GET", "../Controllers/taskController.php?option=7", true);
+                xhr.open("GET", "<?= __DIR__ ?>/Controllers/taskController.php?option=7", true);
                 xhr.send();
             }
         </script>
@@ -73,11 +73,11 @@
     <main>
         <div class="add">
             <button id="saveOrder">
-                <img class="icon" src="../public/assets/diskette.png" alt="">
+                <img class="icon" src="<?= __DIR__ ?>/public/assets/diskette.png" alt="">
                 <span>Save order</span>
             </button>
             <button onclick="openModal()">
-                <img class="icon" src="../public/assets/plus.png" alt="">
+                <img class="icon" src="<?= __DIR__ ?>/public/assets/plus.png" alt="">
                 <span>Nova tarefa</span>
             </button>
         </div>
