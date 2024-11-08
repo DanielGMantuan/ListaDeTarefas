ca249748a667d236cdfd40f88550530aadd0e124
Daniel <daniel.mantuan@itixti.com.br>
2024-11-08 14:44:54 -0300

    teste

--- api/DAOs/tarefaDAO.php
+++ api/DAOs/tarefaDAO.php
@@ -1,7 +1,7 @@
 <?php
-    require_once "../Models/tarefa.php";
-    require_once "conection.inc.php";
-    require_once "../utils/dateConvert.inc.php";
+    require_once __DIR__ . "/../Models/tarefa.php";
+    require_once __DIR__ . "/conection.inc.php";
+    require_once __DIR__ . "/../utils/dateConvert.inc.php";
 
     class TarefaDAO{
         private $con;
