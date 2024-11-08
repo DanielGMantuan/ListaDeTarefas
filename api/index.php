9ab2bca02ec38c105cb568937807ae2146071021
Daniel <daniel.mantuan@itixti.com.br>
2024-11-08 11:38:30 -0300

    fix

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
-    require_once "./Models/tarefa.php";
-    require_once "./utils/dateConvert.inc.php";
-    require_once "./utils/MoneyConversion.php";
+    require_once ( __DIR__  . "/../Models/tarefa.php");
+    require_once ( __DIR__  . "/../utils/dateConvert.inc.php");
+    require_once (__DIR__  . "/../utils/MoneyConversion.php");
 
     session_start();
 
