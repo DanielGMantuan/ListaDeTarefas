1e431c38ee6bec725a0404fab50f71fc9be933ca
Daniel <daniel.mantuan@itixti.com.br>
2024-11-08 10:40:32 -0300

    fix: vercel struct

--- Views/index.php
+++ Views/index.php
@@ -23,10 +23,10 @@
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     
-    <script lang="javascript" src="../Views/scripts/jquery-3.7.1.min.js"></script>
-    <link rel="stylesheet" href="../Views/css/style.css">
-    <script lang="javascript" src="../Views/scripts/datePicker.js"></script>
-    <script lang="javascript" src="../Views/scripts/modal.js"></script>
+    <script lang="javascript" src="./scripts/jquery-3.7.1.min.js"></script>
+    <link rel="stylesheet" href="./css/style.css">
+    <script lang="javascript" src="./scripts/datePicker.js"></script>
+    <script lang="javascript" src="./scripts/modal.js"></script>
 </head>
 <body>
     <?php
